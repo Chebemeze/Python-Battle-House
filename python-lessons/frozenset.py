ed9ed097d90d6e6b234f54112eaad070834774bb
Eze Chukwuchebem Ebenezer <113605198+Chebemeze@users.noreply.github.com>
2026-09-20 11:17:39 +0100

    Implement group_by_tag_combination function
    Clean code that'll group by tags and specify the title(s) thats belongs to the tag

--- python-lessons/frozenset.py
+++ python-lessons/frozenset.py
@@ -1,19 +1,8 @@
-permissions = {frozenset(["admin", "editor"]): "Full Access"}
-permissions = frozenset({"a", "b", "a"})
-print(permissions)
-alt = frozenset(["admin"])
-if permissions == alt:
-    print(permissions)
-#print(permissions)
-
-new_dict = {}
-# def group_by_tag_combination(articles):
-#     if articles is None:
-#         return {}
-#     for index, article in enumerate(articles):
-#         for key, value in article.items():
-#             if title not in new_dict
-
-
-# group_by_tag_combination([{"title": "A", "tags": ["python", "web"]}, {"title": "B", "tags": ["web", "python"]}])
-
+def group_by_tag_combination(articles):
+    same_tags = {}
+    for article in articles:
+        title = article["title"]
+        tag = frozenset(article["tags"])
+        same_tags[tag]= same_tags.get(tag, [])
+        same_tags[tag].append(title)
+    return same_tags
