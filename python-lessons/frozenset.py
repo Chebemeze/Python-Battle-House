1778925a6e0fe5c18783abaed4530d0a657ed777
Chebemeze <Chebemeze@gmail.com>
2026-09-21 14:31:55 +0100

    moved both files to their respective folders

--- python-lessons/frozenset.py
+++
@@ -1,8 +0,0 @@
-def group_by_tag_combination(articles):
-    same_tags = {}
-    for article in articles:
-        title = article["title"]
-        tag = frozenset(article["tags"])
-        same_tags[tag]= same_tags.get(tag, [])
-        same_tags[tag].append(title)
-    return same_tags
